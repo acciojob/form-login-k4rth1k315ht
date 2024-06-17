41f76c950486f5720a51e75d690ee216d39336b9
k4rth1k315ht <163455822+k4rth1k315ht@users.noreply.github.com>
2024-06-18 00:00:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	var firstname=document.getElementById("fname").value;
+var lastname=document.getElementById("lname").value;
+var fullname= firstname + " " + lastname;
 
 }
